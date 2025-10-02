78c37280587058b841eb3036340bf0611c35e195
Denys Bulakh <denys3xd@gmail.com>
2025-10-02 20:55:41 +0200

    6th task solution

--- 06-data-types/start.js
+++ 06-data-types/start.js
@@ -12,3 +12,20 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+let string = 'Hello World';
+let num = 42;
+let boolean = true;
+let nullValue = null;
+let undefinedValue;
+let object = {
+  key: 'value'
+};
+let array = [];
+
+console.log(typeof string);
+console.log(typeof num);
+console.log(typeof boolean);
+console.log(typeof nullValue);
+console.log(typeof undefinedValue);
+console.log(typeof object);
+console.log(typeof array);
