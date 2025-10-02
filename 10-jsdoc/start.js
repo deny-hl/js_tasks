f7de45dd28d9e95aeb8c7a8206452c9764dbf709
Denys Bulakh <denys3xd@gmail.com>
2025-10-02 21:15:53 +0200

    10th task solution

--- 10-jsdoc/start.js
+++ 10-jsdoc/start.js
@@ -12,3 +12,15 @@
  *
  * 4. Вызовите функцию
  */
+// Function that returns the difference between two numbers
+function difference(a, b) {
+  return a - b;
+}
+
+console.log(difference(10, 5));
+/** Return the difference of two numbers
+ * 
+ * @param {number} a - The first number
+ * @param {number} b - The second number
+ * @returns {number} The difference of the two numbers
+ */
